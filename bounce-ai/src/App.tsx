4d027b4f27f4693bb7acadc9830cc90db3a441b4
atsoc1993 <atsoc1993@gmail.com>
2025-12-29 02:25:53 -0500

    Added Canvas, ambient/spot lighting, and physics from rapier w/ a sphere & plane; there sphere currently has bounciness via restitution property and behaving radically when bouncing. Will need some sort of boundary to prevent it from going out of bounds from the camera. Could also move the camera outwards more and make the objective for the plane to try to keep the ball centered in the render

--- bounce-ai/src/App.tsx
+++ bounce-ai/src/App.tsx
@@ -1,3 +1,6 @@
+import { Canvas } from "@react-three/fiber";
+import { Physics, RigidBody } from "@react-three/rapier";
+
 export default function App() {
   return (
     <div className="bg-green-200 h-full w-full p-5">
@@ -6,7 +9,25 @@ export default function App() {
           <div className="shadow-green-300 shadow-2xl h-full w-full p-5">
             <div className="shadow-green-300 shadow-2xl h-full w-full p-5">
               <div className="shadow-green-300 shadow-2xl h-full w-full p-5">
-
+                <Canvas
+                  camera={{ position: [0, 5, 20] }}>
+                  <ambientLight intensity={0.5} />
+                  <spotLight position={[10, 10, 3]} intensity={200} angle={Math.PI / 6} />
+                  <Physics gravity={[0, -9.81, 0]}>
+                    <RigidBody position={[0, 10, 0]} restitution={2}>
+                      <mesh>
+                        <sphereGeometry args={[1, 50, 50]} />
+                        <meshStandardMaterial color="white" />
+                      </mesh>
+                    </RigidBody>
+                    <RigidBody type="fixed">
+                      <mesh>
+                        <boxGeometry args={[10, 1, 10]} />
+                        <meshStandardMaterial color="white" />
+                      </mesh>
+                    </RigidBody>
+                  </Physics>
+                </Canvas>
               </div>
             </div>
           </div>
